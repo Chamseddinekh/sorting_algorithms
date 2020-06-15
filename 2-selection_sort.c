0e38ed938165fc969dc0e65289158697c76513ba
Chamseddinekh <chamsseddine.khedhri@gmail.com>
2020-06-15 17:04:59 +0100

    secelct

---
+++ 2-selection_sort.c
@@ -0,0 +1,23 @@
+#include "sort.h"
+/**
+ * selection_sort - sorts an array of integers in ascending order
+ *
+ * @array: The array to be sorted
+ * @size: Number of elements in @array
+ */
+void selection_sort(int *array, size_t size)
+{
+size_t i;
+int j, tmp;
+for (i = 0; i < size; i++)
+{
+tmp = array[i];
+j = i - 1;
+while (j >= 0 && array[j] > tmp)
+{
+array[j + 1] = array[j];
+j = j - 1;
+array[j + 1] = tmp;
+}
+}
+}
