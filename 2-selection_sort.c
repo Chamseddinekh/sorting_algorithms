5347a2dabdddc1f876a7f4f7a4a20871c9720ae2
Chamseddinekh <chamsseddine.khedhri@gmail.com>
2020-06-16 11:40:05 +0100

    sort fixed

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -12,9 +12,9 @@ size_t i, j, tmp, min;
 for (i = 0; i < size - 1; i++)
 {
 min = i;
-for(j = i + 1; j < size; j++)
+for (j = i + 1; j < size; j++)
 {
-if(array[j] < array[min])
+if (array[j] < array[min])
 {
 min = j;
 }
